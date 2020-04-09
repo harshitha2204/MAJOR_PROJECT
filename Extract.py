f85ab4414f994b129ba8903d625fd28a1f57936b
harinirao1998 <30483498+harinirao1998@users.noreply.github.com>
2020-04-09 20:13:05 +0530

    Create Extract.py

---
+++ Extract.py
@@ -0,0 +1,45 @@
+import fnmatch
+import os
+import zipfile
+
+
+
+
+def extract_files(zip_file, out_dir, delete_zip=False):
+        
+    audio_dir = os.path.join(out_dir, 'audio')
+    if not os.path.exists(audio_dir):
+        os.makedirs(audio_dir)
+
+    
+    transcripts_dir = os.path.join(out_dir, 'transcripts')
+    if not os.path.exists(audio_dir):
+        os.makedirs(transcripts_dir)
+
+    zip_ref = zipfile.ZipFile(zip_file)
+    for f in zip_ref.namelist():  # iterate through files in zip file
+        if f.endswith('.wav'):
+            zip_ref.extract(f, audio_dir)
+        elif fnmatch.fnmatch(f, '*TRANSCRIPT.csv'):
+            zip_ref.extract(f, transcripts_dir)
+    zip_ref.close()
+
+    if delete_zip:
+        os.remove(zip_file)
+
+
+if __name__ == '__main__':
+   
+    dir_name = 'C:\\Users\\Harini\\Downloads\\depression-detect-master\\src\\DAIC-WOZ'
+
+    
+    out_dir = 'C:\\Users\\Harini\\Downloads\\depression-detect-master\\src\\raw'
+
+    
+    delete_zip = False
+
+    
+    for file in os.listdir(dir_name):
+        if file.endswith('.zip'):
+            zip_file = os.path.join(dir_name, file)
+            extract_files(zip_file, out_dir, delete_zip=delete_zip)
