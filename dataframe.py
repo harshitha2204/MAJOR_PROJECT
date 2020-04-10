305fd18033940a871de7cf7e48e94a4a9072674e
harshitha2204 <30696277+harshitha2204@users.noreply.github.com>
2020-04-10 09:43:43 +0530

    Create dataframe.py

---
+++ dataframe.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+df_train = pd.read_csv('../../data/raw/labels/train_split_Depression_AVEC2017.csv')
+
+df_test = pd.read_csv('../../data/raw/labels/ådev_split_Depression_AVEC2017.csv')
+
+df_dev = pd.concat([df_train, df_test], axis=0)
+
+print(df_dev)
