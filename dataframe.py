81a17f04b1e8438931570bcb40f69d86f2ad77e4
harshitha2204 <30696277+harshitha2204@users.noreply.github.com>
2020-04-10 09:44:47 +0530

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-df_train = pd.read_csv('../../data/raw/labels/train_split_Depression_AVEC2017.csv')
+df_train = pd.read_csv('/Home/Documents/depression-detection/src/raw/labels/train_split_Depression_AVEC2017.csv')
 
-df_test = pd.read_csv('../../data/raw/labels/ådev_split_Depression_AVEC2017.csv')
+df_test = pd.read_csv('/Home/Documents/depression-detection/src/raw/labels/ådev_split_Depression_AVEC2017.csv')
 
 df_dev = pd.concat([df_train, df_test], axis=0)
 
